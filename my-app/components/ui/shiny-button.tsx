2a0cb0c9129d0a613d0915418fb2b598612bfb6f
KiranBabu007 <kiranbabu3355@gmail.com>
2025-03-16 15:28:21 +0530

    refactor: enhance ShinyButton styles; update padding, background gradient, and text properties

--- my-app/components/ui/shiny-button.tsx
+++ my-app/components/ui/shiny-button.tsx
@@ -39,15 +39,20 @@ const ShinyButton = React.forwardRef<HTMLButtonElement, ShinyButtonProps>(
         {...animationProps}
         {...props}
         className={cn(
-          "relative rounded-lg px-6 py-2 font-medium backdrop-blur-xl transition-shadow duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]",
-          className,
+          "relative rounded-lg px-6 py-3 font-medium backdrop-blur-xl",
+          "bg-gradient-to-r from-purple-800",
+          "hover:from-purple-500 hover:via-pink-600 hover:to-violet-950 hover:text-white",
+          "text-white shadow-lg shadow-purple-950/50",
+          "transition-all duration-300",
+          "flex items-center gap-2",
+          className
         )}
       >
         <span
-          className="relative block size-full text-sm uppercase tracking-wide font-light text-black"
+          className="relative block size-full text-base uppercase tracking-wide font-semibold"
           style={{
             maskImage:
-              "linear-gradient(-75deg,hsl(var(--primary)) calc(var(--x) + 20%),transparent calc(var(--x) + 30%),hsl(var(--primary)) calc(var(--x) + 100%))",
+              "linear-gradient(-75deg,rgba(255,255,255,1) calc(var(--x) + 20%),transparent calc(var(--x) + 30%),rgba(255,255,255,1) calc(var(--x) + 100%))",
           }}
         >
           {children}
@@ -57,11 +62,11 @@ const ShinyButton = React.forwardRef<HTMLButtonElement, ShinyButtonProps>(
             mask: "linear-gradient(rgb(0,0,0), rgb(0,0,0)) content-box,linear-gradient(rgb(0,0,0), rgb(0,0,0))",
             maskComposite: "exclude",
           }}
-          className="absolute inset-0 z-10 block rounded-[inherit] bg-[linear-gradient(-75deg,hsl(var(--primary)/10%)_calc(var(--x)+20%),hsl(var(--primary)/50%)_calc(var(--x)+25%),hsl(var(--primary)/10%)_calc(var(--x)+100%))] p-px"
+          className="absolute inset-0 z-10 block rounded-[inherit] bg-[linear-gradient(-75deg,rgba(139,92,246,0.3)_calc(var(--x)+20%),rgba(139,92,246,0.7)_calc(var(--x)+25%),rgba(139,92,246,0.3)_calc(var(--x)+100%))] p-px"
         ></span>
       </motion.button>
     );
-  },
+  }
 );
 
 ShinyButton.displayName = "ShinyButton";
